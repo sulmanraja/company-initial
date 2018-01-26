2c7bd6d372c36a49ccb230cbefc5a31b8e01b2d6
sulmanraja <sulman.raja@gmail.com>
2018-01-26 08:38:05 -0600

    enable eureka

--- src/main/java/io/rscale/training/company/CompanyApplication.java
+++ src/main/java/io/rscale/training/company/CompanyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class CompanyApplication {
 
 	public static void main(String[] args) {
